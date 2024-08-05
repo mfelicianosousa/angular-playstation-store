d2c1767d36abc56d7b08f91cb8c6a9688827709c
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-08-05 15:51:02 -0400

    Playstation Store

---
+++ src/app/components/card/card-label/card-label.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-label',
+  templateUrl: './card-label.component.html',
+  styleUrl: './card-label.component.css'
+})
+export class CardLabelComponent {
+
+	@Input()
+	gameLabel: string ="";
+
+}
